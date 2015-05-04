a6c807a548ab32185f9352add9f9caa36db37f56
Jian Lin <linjian815@gmail.com>
2015-05-04 15:44:58 +0800

    添加seed数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+%w[保养 维修 发动机 保险 其他].each do |tag_name|
+  Tag.find_or_create_by(name: tag_name)
+end
