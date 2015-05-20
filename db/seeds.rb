dd8bdf7801a23eb3658cc2bfd916c5238f3b500d
Jian Lin <linjian815@gmail.com>
2015-05-20 14:32:59 +0800

    添加预定义标签

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 %w[dispatcher support_manager engineer specialist manager].each do |role|
   Role.find_or_create_by!(name: role)
 end
+
+['用车保养', '故障维修', '车险交规', '美容外观', '新车 二手车', '其他'].each do |tag|
+  ActsAsTaggableOn::Tag.find_or_create_by!(name: tag)
+end
