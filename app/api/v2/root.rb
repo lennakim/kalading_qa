e79372e5ec207cec24769e01c4971017dc81285c
shooter <shooter.hero@gmail.com>
2015-05-21 16:24:53 +0800

    去除后缀

--- app/api/v2/root.rb
+++ app/api/v2/root.rb
@@ -14,6 +14,6 @@ module V2
       header['Access-Control-Request-Method'] = '*'
     end
 
-    add_swagger_documentation base_path: "/api", api_version: 'v2', mount_path: 'doc.json'
+    add_swagger_documentation base_path: "/api", api_version: 'v2', mount_path: 'doc'
   end
 end
