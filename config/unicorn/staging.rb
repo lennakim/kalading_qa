96495b682ce5e5af773ea2336c82277829677157
Jian Lin <linjian815@gmail.com>
2015-05-25 16:23:43 +0800

    补充config/unicorn/staging.rb

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,3 +1,57 @@
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete documentation.
+
+# root_path = File.expand_path '../', File.dirname(__FILE__)
+root_path = "/home/deployer/apps/kalading_qa/current"
+working_directory root_path
+
 listen "/tmp/unicorn_kalading_qa.sock", backlog: 64
 worker_processes 2
 timeout 30
+
+pid "#{root_path}/tmp/pids/unicorn.pid"
+stderr_path "#{root_path}/log/unicorn.stderr.log"
+stdout_path "#{root_path}/log/unicorn.stdout.log"
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
+
+preload_app true
+
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  # The following is only recommended for memory/DB-constrained
+  # installations.  It is not needed if your system can house
+  # twice as many worker_processes as you have configured.
+  #
+  # # This allows a new master process to incrementally
+  # # phase out the old master process with SIGTTOU to avoid a
+  # # thundering herd (especially in the "preload_app false" case)
+  # # when doing a transparent upgrade.  The last worker spawned
+  # # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
