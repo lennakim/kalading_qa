aca02bc9679b6cba47448e65c18b350e69e16097
Jian Lin <linjian815@gmail.com>
2015-05-25 14:27:10 +0800

    staging deployment

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'kalading_qa'
+set :repo_url, 'git@bitbucket.org:kalading_team/kalading_qa.git'
+set :deploy_user, 'deployer'
+set :scm, :git
+
+# rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.5'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+    invoke 'unicorn:legacy_restart'
+  end
+  after 'deploy:publishing', 'deploy:restart'
+
+  after :finishing, 'deploy:cleanup'
+end
