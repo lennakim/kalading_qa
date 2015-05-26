c21b2bf914a46efb0e761afc1764347baa128405
Jian Lin <linjian815@gmail.com>
2015-05-26 14:28:08 +0800

    添加用户登录说明 修改seed数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,37 @@ end
 
 
 # 创建客服登录帐号
-if !User.where(phone_num: '18655551000').exists?
-  dispatcher = User.create!(
-    phone_num: '18655551001',
-    password: '1'*6,
-    name: '客服',
-    name_pinyin: 'kefu',
-    internal_id: 'internal_1000'
-  )
-  dispatcher.assign_roles('dispatcher')
-end
+# if !User.where(phone_num: '18655551000').exists?
+  # dispatcher = User.create!(
+    # phone_num: '18655551001',
+    # password: '1'*6,
+    # name: '客服',
+    # name_pinyin: 'kefu',
+    # internal_id: 'internal_1000'
+  # )
+  # dispatcher.assign_roles('dispatcher')
+# end
 
 # 创建客服主管登录帐号
-if !User.where(phone_num: '18655552000').exists?
-  support_manager = User.create!(
-    phone_num: '18655552001',
-    password: '1'*6,
-    name: '客服主管',
-    name_pinyin: 'kefuzhuguan',
-    internal_id: 'internal_2000'
-  )
-  support_manager.assign_roles('support_manager')
-end
+# if !User.where(phone_num: '18655552000').exists?
+  # support_manager = User.create!(
+    # phone_num: '18655552001',
+    # password: '1'*6,
+    # name: '客服主管',
+    # name_pinyin: 'kefuzhuguan',
+    # internal_id: 'internal_2000'
+  # )
+  # support_manager.assign_roles('support_manager')
+# end
 
 # 创建总裁登录帐号
-if !User.where(phone_num: '18655555000').exists?
-  manager = User.create!(
-    phone_num: '18655555001',
-    password: '1'*6,
-    name: '总裁',
-    name_pinyin: 'zongcai',
-    internal_id: 'internal_5000'
-  )
-  manager.assign_roles('manager')
-end
+# if !User.where(phone_num: '18655555000').exists?
+  # manager = User.create!(
+    # phone_num: '18655555001',
+    # password: '1'*6,
+    # name: '总裁',
+    # name_pinyin: 'zongcai',
+    # internal_id: 'internal_5000'
+  # )
+  # manager.assign_roles('manager')
+# end
